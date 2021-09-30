ed615081752a506687bf22807e3be3be8861d3af
TrentDwyer1 <90116966+TrentDwyer1@users.noreply.github.com>
2021-09-30 07:58:34 -0400

    Add files via upload
    I was unable to add files, using publish. I have updated those files I edited.

---
+++ lab4/Student.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Student extends Component {
+    
+  render() { 
+    return (
+        //constuctor
+        react.createElement("name", props, "Trent Dwyer");
+        react.createElement("ID", props, 10);
+        <div>
+            <p>{this.props.name}</p>
+            <p>{this.props.ID}</p>
+        </div>);
+  }
+  
+} export default Student;
