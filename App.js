bf01aa2b85c25d137079210151390b59fdb24002
TrentDwyer1 <90116966+TrentDwyer1@users.noreply.github.com>
2021-09-30 07:54:42 -0400

    Add files via upload
    I was only able to add directly through online, publish would not direct to the correct place.

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import Student from './Student';
+
+export default class App extends Component {
+  render() {
+    return(
+      <Student/>
+    )
+    
+  }
+}
