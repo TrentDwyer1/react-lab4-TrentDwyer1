e68e53fe8c203785c2419641892849a9f14c5f13
TrentDwyer1 <90116966+TrentDwyer1@users.noreply.github.com>
2021-09-30 08:05:51 -0400

    Update Student.js

--- lab4/src/Student.js
+++ lab4/src/Student.js
@@ -5,8 +5,13 @@ class Student extends Component {
   render() { 
     return (
         //constuctor
-        react.createElement("name", props, "Trent Dwyer");
-        react.createElement("ID", props, 10);
+        constructor(props){
+            this.props = {
+            react.createElement("name", props, "Trent Dwyer");
+            react.createElement("ID", props, 10);
+        
+        }
+
         <div>
             <p>{this.props.name}</p>
             <p>{this.props.ID}</p>
